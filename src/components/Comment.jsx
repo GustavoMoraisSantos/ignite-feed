b58d712e471b9cb0c30aa6f5b94332c48b6be0fc
Gustavo Morais Lopes dos Santos <gustavo.morais1414@gmail.com>
2024-06-18 15:25:02 -0300

    feat(comments): like comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,10 +2,17 @@
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import Avatar from "./Avatar";
 import styles from "./Comment.module.css";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -30,11 +37,11 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <div>
               <ThumbsUp />
               Aplaudir
-              <span>20</span>
+              <span>{likeCount}</span>
             </div>
           </button>
         </footer>
